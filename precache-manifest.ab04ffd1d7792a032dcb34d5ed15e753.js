f7a50a689d3234d7758a37abbe059fa7a2eefb18
Rishiraj Acharya <44090649+rishiraj@users.noreply.github.com>
2020-12-23 16:51:04 +0530

    Add files via upload

---
+++ precache-manifest.ab04ffd1d7792a032dcb34d5ed15e753.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f93cc40341fb51b5a6326fe2ec166b0f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "1f7cb833297ebd33526f",
+    "url": "/static/css/main.9499ae93.chunk.css"
+  },
+  {
+    "revision": "507f7f772c60cf13d951",
+    "url": "/static/js/2.f1c34aac.chunk.js"
+  },
+  {
+    "revision": "3fee6343beae956928494854364befaf",
+    "url": "/static/js/2.f1c34aac.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1f7cb833297ebd33526f",
+    "url": "/static/js/main.1e446f3c.chunk.js"
+  },
+  {
+    "revision": "0607b5ce656469f45f78",
+    "url": "/static/js/runtime-main.cee0cca5.js"
+  }
+]);
