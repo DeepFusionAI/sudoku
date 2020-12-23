8c8fc385047cd1af3331c6ce6be5e288aad4a78e
Rishiraj Acharya <44090649+rishiraj@users.noreply.github.com>
2020-12-23 23:50:00 +0530

    Update precache-manifest.ab04ffd1d7792a032dcb34d5ed15e753.js

--- precache-manifest.ab04ffd1d7792a032dcb34d5ed15e753.js
+++ precache-manifest.ab04ffd1d7792a032dcb34d5ed15e753.js
@@ -1,26 +1,26 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
     "revision": "f93cc40341fb51b5a6326fe2ec166b0f",
-    "url": "/index.html"
+    "url": "/sudoku/index.html"
   },
   {
     "revision": "1f7cb833297ebd33526f",
-    "url": "/static/css/main.9499ae93.chunk.css"
+    "url": "/sudoku/static/css/main.9499ae93.chunk.css"
   },
   {
     "revision": "507f7f772c60cf13d951",
-    "url": "/static/js/2.f1c34aac.chunk.js"
+    "url": "/sudoku/static/js/2.f1c34aac.chunk.js"
   },
   {
     "revision": "3fee6343beae956928494854364befaf",
-    "url": "/static/js/2.f1c34aac.chunk.js.LICENSE.txt"
+    "url": "/sudoku/static/js/2.f1c34aac.chunk.js.LICENSE.txt"
   },
   {
     "revision": "1f7cb833297ebd33526f",
-    "url": "/static/js/main.1e446f3c.chunk.js"
+    "url": "/sudoku/static/js/main.1e446f3c.chunk.js"
   },
   {
     "revision": "0607b5ce656469f45f78",
-    "url": "/static/js/runtime-main.cee0cca5.js"
+    "url": "/sudoku/static/js/runtime-main.cee0cca5.js"
   }
 ]);
